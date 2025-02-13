526cec4f9bbd3faed707223f6978b7b1dd9da025
unknown <sesarazua@scl.edu.gt>
2025-02-13 13:31:34 -0600

    Se agrega la animación de voltear cartas y se modifican los archivos:     - tablero.js     - tablero.css

--- components/tablero/tablero.js
+++ components/tablero/tablero.js
@@ -1,16 +1,32 @@
 import { mezclarCartas } from "./data.js";
 
-function item(contenido){
-
+function item(contenido) {
     let div = document.createElement('div');
     div.className = "carta-item";
-    div.innerText = contenido;
+
+    // Crear la cara frontal (oculta el contenido)
+    let frente = document.createElement('div');
+    frente.className = "cara frente";
+    frente.innerText = "?"; // Puedes cambiar esto por una imagen si lo deseas
+
+    // Crear la cara trasera (muestra la fruta cuando se voltea)
+    let atras = document.createElement('div');
+    atras.className = "cara atras";
+    atras.innerText = contenido;
+
+    // Agregar las caras al div principal de la carta
+    div.appendChild(frente);
+    div.appendChild(atras);
+
+    // Evento para voltear la carta
+    div.addEventListener("click", () => {
+        div.classList.toggle("volteada");
+    });
 
     return div;
 }
 
 function cargarCartas() {
-    
     let div = document.createElement('div');
     div.className = "tablero-div";
 
@@ -19,9 +35,6 @@ function cargarCartas() {
     });
 
     return div;
-
-    
-
-};
+}
 
 export { cargarCartas };
